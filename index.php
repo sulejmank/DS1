0f4c435a957760812da927955c9262de3677e94f
sulejmank <6103201@gmail.com>
2017-10-21 23:46:04 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,79 @@
+<?php
+session_start();
+require_once("main/main.php");
+?>
+
+<!doctype html>
+<html>
+	<head>
+
+		<title>Vešanje</title>
+		<link rel="stylesheet" href="https://bootswatch.com/paper/bootstrap.min.css">
+		<link type="text/css" rel="stylesheet" href="css/style.css">
+
+	</head>
+<body>
+	<h3 style = "text-align: center;">Vešanje</h3>
+	<hr />
+	<div class='vesanje jumbotron'>
+		<img <?php echo "src='media/".$_SESSION['brPogresnihPokusaja'].".png' width='150px'  "; ?> ><br />
+	</div>
+
+<?php
+if($rezultat== -1)
+	echo "<h6 style='text-align:center'><strong>Igra zavrsena! Trazena rec je bila : ".$rec. "</strong></h6>";
+elseif ($rezultat== 1)
+	echo "<h5 style='text-align:center'><strong>Bravoo! :) </strong></h5>";
+
+?>
+
+<div class="input_area">
+
+<?php
+for($i=0; $i<$word_size; $i++){
+
+	$g="";
+	if(isset($guessed_char)){
+		if(in_array($word_chars[$i], $guessed_char)){
+			$g=$word_chars[$i];
+		}else{
+		}
+	}
+	echo "<input type='text' value='$g' size='1' name='$i'  maxlength='1' class='guessed' readonly />";
+}
+?>
+
+</div>
+<div class="tastatura ">
+<?php
+
+$tastatura = range('A', 'Z');
+foreach($tastatura as $char){
+$dbl = "";
+	if(isset($_POST['guess_it'])){
+		if(in_array($char, $guessed_char)){
+			$dbl="disabled";
+		}else{
+			$dbl="";
+		}
+	}
+	if($rezultat== -1 || $rezultat== 1) $dbl="disabled";
+	echo "<span><form  action='#' method='post'>
+				<input type='hidden' value='$char' name='guess'  />
+				<input type='submit' value='$char' name='guess_it' class='buttkey btn-default' $dbl />
+				</form></span>";
+}
+
+if(isset($_POST['restart'])){
+	session_destroy();
+	echo "<meta http-equiv='refresh' content='0'>";
+}
+?>
+<br /><br />
+	<form action="#" method="post">
+		<input type="submit" value=" Nova Reč " name="restart" class="btn btn-primary btn-lg" />
+	</form>
+</div>
+
+	</body>
+</html>
---
+++ main/main.php
@@ -0,0 +1,58 @@
+<?php
+
+$recnik = explode(" ", file_get_contents("reci.txt"));
+
+ $max_pokusaj = 7;
+
+ $velicina_recnika = count($recnik);
+
+if(!isset($_SESSION['rec_id'])) {
+	$random_id = rand(0, $velicina_recnika-2);
+	$_SESSION['rec_id'] = 	$random_id ;
+	$_SESSION['brPogresnihPokusaja']= 0;
+	$_SESSION['user_guesses']= array();
+
+} else {
+	$random_id = $_SESSION['rec_id'];
+}
+
+
+$rec = $recnik[$random_id];
+$rec_id = $random_id;
+$rec= strtoupper($rec);
+
+$word_size = strlen($rec);
+$word_chars = str_split($rec);
+$word_char_unique = array_unique($word_chars);
+$rezultat = 0;
+
+
+if(isset($_POST['guess_it'])){
+
+	$user_guess = $_POST['guess'];
+	array_push($_SESSION['user_guesses'], $user_guess);
+	$guessed_char = $_SESSION['user_guesses'];
+
+	if(!in_array($user_guess, $word_chars)){
+		$_SESSION['brPogresnihPokusaja']++;
+	}
+	$rezultat = vidi_rezultat();
+}
+
+
+function vidi_rezultat(){
+
+	global $word_char_unique;
+	global $max_pokusaj;
+
+	$rezultat = array_intersect($_SESSION['user_guesses'], $word_char_unique);
+	if(count($rezultat) == count($word_char_unique)){
+
+			return $rezultat = 1;
+	}
+
+	if($_SESSION['brPogresnihPokusaja'] < $max_pokusaj )
+		return $rezultat =0;
+	 else
+		return $rezultat = -1;
+}
